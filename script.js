1b9a6302bee0d727a73fb0e2549b52a222874a37
zane108231 <johnlongno4@gmail.com>
2024-08-12 20:14:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,23 @@
-function getHistory(){
-  return document.getElementById("history-value").innerText;
-}
-function printHistory(num){
-  document.getElementById("history-value").innerText=num;
-}
-function getOutput(){
-  return document.getElementById("output-value").innerText;
-}
-function printOutput(num){
-  if(num==""){
-    document.getElementById("output-value").innerText=num;
-  }
-  else{
-    document.getElementById("output-value").innerText=getFormattedNumber(num);
-  } 
-}
-function getFormattedNumber(num){
-  if(num=="-"){
-    return "";
-  }
-  var n = Number(num);
-  var value = n.toLocaleString("en");
-  return value;
-}
-function reverseNumberFormat(num){
-  return Number(num.replace(/,/g,''));
-}
-var operator = document.getElementsByClassName("operator");
-for(var i =0;i<operator.length;i++){
-  operator[i].addEventListener('click',function(){
-    if(this.id=="clear"){
-      printHistory("");
-      printOutput("");
-    }
-    else if(this.id=="backspace"){
-      var output=reverseNumberFormat(getOutput()).toString();
-      if(output){//if output has a value
-        output= output.substr(0,output.length-1);
-        printOutput(output);
-      }
-    }
-    else{
-      var output=getOutput();
-      var history=getHistory();
-      if(output==""&&history!=""){
-        if(isNaN(history[history.length-1])){
-          history= history.substr(0,history.length-1);
-        }
-      }
-      if(output!="" || history!=""){
-        output= output==""?output:reverseNumberFormat(output);
-        history=history+output;
-        if(this.id=="="){
-          var result=eval(history);
-          printOutput(result);
-          printHistory("");
-        }
-        else{
-          history=history+this.id;
-          printHistory(history);
-          printOutput("");
-        }
-      }
-    }
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
 
-  });
-}
-var number = document.getElementsByClassName("number");
-for(var i =0;i<number.length;i++){
-  number[i].addEventListener('click',function(){
-    var output=reverseNumberFormat(getOutput());
-    if(output!=NaN){ //if output is a number
-      output=output+this.id;
-      printOutput(output);
-    }
-  });
-}
-
-var equalsButton = document.getElementById("=");
-equalsButton.addEventListener('click', function() {
-  printILY();
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I love you too! 😘";
+  gif.src =
+    "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
 
-function printILY(){
-  document.getElementById("output-value").innerText="imisssuuu :(";
-}
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
